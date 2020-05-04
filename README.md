93a6d8e2e07caaede1ff9e0645313b37f3e055ae
James Ng <james.ng@projapps.com>
2020-05-04 19:04:46 +0800

    Completed Stage #2

---
+++ README.md
@@ -0,0 +1,20 @@
+# Project: Text-Based Browser
+### About
+Sometimes you need to read online documentation or find something on the Internet from the command line or terminal. So, let's use Python to create a text-based browser! Of course, making a real, full-blown browser is a very difficult task. In this project, you'll create a very simple browser that will ignore JavaScript and CSS, won't have cookies, and will only process a limited set of tags. Still, it will be useful and, most importantly, fun to program!
+### Learning outcomes
+You will learn how HTTP works and how to handle its protocols using Python. You will also learn all about Python input/output, as well as parsing HTML.
+### This project is a part of the following learning goals
+Python Developer
+## What you’ll do and what you’ll learn
+### Stage 1/6: Address line
+Let's create the first capability for our browser! It has to read the URL and show the user a "hard-coded" website with some news.
+### Stage 2/6: Tabs
+Time to add some user-friendliness. The program should store our pages as files and show them if the user enters a shortened request. Watch for inappropriate input, though!
+### Stage 3/6: Hotkeys
+Sometimes you want to revisit the past. Let's add a “back” button by using the stack.
+### Stage 4/6: Requesting
+Time to play in a new way. Now only real web pages! Get to know the Request library.
+### Stage 5/6: Parsing
+Let’s enhance the readability of our pages and teach our browser to output only a certain text from HTML tags.
+### Stage 6/6: Formatted output
+Look-and-feel is important to the browser experience. Let's read about a new library, experiment a bit, and highlight our links in blue.
---
+++ Text-Based Browser/task/browser/browser.py
@@ -0,0 +1,80 @@
+import os
+import sys
+
+nytimes_com = '''
+This New Liquid Is Magnetic, and Mesmerizing
+
+Scientists have created “soft” magnets that can flow 
+and change shape, and that could be a boon to medicine 
+and robotics. (Source: New York Times)
+
+
+Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
+
+Jessica Wade has added nearly 700 Wikipedia biographies for
+ important female and minority scientists in less than two 
+ years.
+
+'''
+
+bloomberg_com = '''
+The Space Race: From Apollo 11 to Elon Musk
+
+It's 50 years since the world was gripped by historic images
+ of Apollo 11, and Neil Armstrong -- the first man to walk 
+ on the moon. It was the height of the Cold War, and the charts
+ were filled with David Bowie's Space Oddity, and Creedence's 
+ Bad Moon Rising. The world is a very different place than 
+ it was 5 decades ago. But how has the space race changed since
+ the summer of '69? (Source: Bloomberg)
+
+
+Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
+
+Twitter and Square Chief Executive Officer Jack Dorsey 
+ addressed Apple Inc. employees at the iPhone maker’s headquarters
+ Tuesday, a signal of the strong ties between the Silicon Valley giants.
+'''
+
+
+def open_page(path, url):
+    filename = path + '/' + url + '.txt'
+    if not os.path.exists(filename):
+        print("Error: Incorrect URL")
+    else:
+        with open(filename) as infile:
+            print(infile.read())
+
+
+def save_page(path, url, text):
+    index = url.rfind('.')
+    filename = path + '/' + url[0:index] + '.txt'
+    with open(filename, 'w') as outfile:
+        outfile.write(text)
+
+
+# write your code here
+path = '.'
+if len(sys.argv) > 1:
+    # create directory
+    directory = sys.argv[1]
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    path = directory
+
+while True:
+    url = input()
+    if url == "exit":
+        break
+    elif url.rfind('.') < 0:
+        open_page(path, url)
+    else:
+        if url == "bloomberg.com":
+            text = bloomberg_com
+        elif url == "nytimes.com":
+            text = nytimes_com
+        else:
+            print("Error: Incorrect URL")
+            continue
+        print(text)
+        save_page(path, url, text)
